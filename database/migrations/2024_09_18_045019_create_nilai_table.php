686d8eaf9526d2ebba4ce1a385a167bdd55864e2
Fajri Rinaldi Chan <fajri@gariskode.com>
2025-02-24 10:45:43 +0700

    fix

--- database/migrations/2024_09_18_045019_create_nilai_table.php
+++ database/migrations/2024_09_18_045019_create_nilai_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->foreign('nim')->references('nim')->on('mahasiswa')->onUpdate('cascade')->onDelete('cascade');
             $table->string('kode_kelas', 10);
             $table->foreign('kode_kelas')->references('kode_kelas')->on('kelas')->onUpdate('cascade')->onDelete('cascade');
+            $table->string('nilai_pretest', 3)->nullable();
             $table->string('nilai_uts', 3)->nullable();
             $table->string('nilai_uas', 3)->nullable();
             $table->string('nilai_tugas', 3)->nullable();
